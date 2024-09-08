bf1af09291fee7b83911c035396519253e3ba382
Natidev <53361365+Natidev@users.noreply.github.com>
2024-09-09 00:22:14 +0300

    scroll effect done

---
+++ script.js
@@ -0,0 +1,11 @@
+let deltascroll=0
+window.addEventListener('scroll',(e)=>{
+    const navbar=document.getElementById("navbar")
+    let diff=Math.abs(window.scrollY-deltascroll)
+    if(diff>100){
+        if((window.scrollY-deltascroll)>0)
+                navbar.classList.add("hide")
+        deltascroll=window.scrollY
+    }else if((window.scrollY-deltascroll)<0)
+        navbar.classList.remove('hide')
+})
