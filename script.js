51dee1db511e312560cc47139398309a55f64c42
Natidev <53361365+Natidev@users.noreply.github.com>
2024-09-09 20:12:22 +0300

    completed credentials page

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 let deltascroll=0
+const loginref=document.getElementById("2login")
+const signupref=document.getElementById("2signup")
+loginref.addEventListener('click',(e)=>{
+    document.getElementById('login').classList.remove('hide')
+    document.getElementById('signup').classList.add('hide')
+
+})
+signupref.addEventListener('click',(e)=>{
+    document.getElementById('login').classList.add('hide')
+    document.getElementById('signup').classList.remove('hide')
+
+})
 window.addEventListener('scroll',(e)=>{
     const navbar=document.getElementById("navbar")
     let diff=Math.abs(window.scrollY-deltascroll)
